cc7bac1b5d4c27b1e403f478f261a600d1e1002f
kr2002 <45427428+kr2002@users.noreply.github.com>
2019-02-16 14:35:25 -0500

    Add files via upload

--- HelmholtzObj.m
+++ HelmholtzObj.m
@@ -26,7 +26,7 @@ for ks=1:Ns
     %axis square; axis off; shading interp;
     %drawnow;
     
-    measR=meas(:,ks)+1e-16;
+    measR=meas(:,ks);
     %rz=(pred-measR')./measR';
     rz=(pred-measR');
     % calculate the objective function (the part for source ks)
@@ -45,14 +45,27 @@ for ks=1:Ns
     
 end
 
-% add regularization
-beta=1e-16; % the regularization parameter
+beta=1e-16; % the regularization parameter, change accoring to noise level
+
+% Add regularization to the objective function
 [Gx,Gy] = pdegrad(P,T,refc);
 Gx1=pdeprtni(P,T,Gx); Gy1=pdeprtni(P,T,Gy);
 f=f+0.5*beta*sum(Gx1.^2+Gy1.^2)*dx*dy;
-if nargout >1
+
+% Add regularization term to the gradient
+if nargout >1   
+    % The Laplacian part
     [Gxx, Gxy]=pdegrad(P,T,Gx1); [Gyx, Gyy]=pdegrad(P,T,Gy1);
     Gx2=pdeprtni(P,T,Gxx); Gy2=pdeprtni(P,T,Gyy);
     DeltaGamma=Gx2+Gy2;
     g=g-beta*DeltaGamma*dx*dy;
+    
+    % The boundary integral part: this part should be removed if boundary 
+    %      value of n is not to be reconstructed
+    dndn=zeros(1,Nd);
+    for j=1:Nd
+        dndn(j)=Gx1(detinfo(1,j))*detinfo(3,j)+Gy1(detinfo(1,j))*detinfo(4,j);
+    end
+    g=g+beta*sum(dndn.*detinfo(5,:));
+
 end
